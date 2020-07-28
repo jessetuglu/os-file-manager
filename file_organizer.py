ed2caa5e8e0f8392a5c65ad3832a4ff402669640
Jesse Tuglu <jessetuglu@Jesses-MBP.local>
2020-07-28 16:33:17 -0400

    original

--- file_organizer.py
+++ file_organizer.py
@@ -1,55 +1,38 @@
-
 import os
 from pathlib import Path
 
-DIRECTORIES = {
-   "HTML": [".html5", ".html", ".htm", ".xhtml"],
-   "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",
-   ".heif", ".psd"],
-   "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng",
+directories = {
+    "PYTHON":['.py'],
+    "PLAINTEXT":['.txt'],
+    "VIDEOS":['.avi','.flv','.wmv',".mov", ".mp4", ".webm", ".vob", ".mng",
    ".qt", ".mpg", ".mpeg", ".3gp"],
-   "DOCUMENTS": [".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods",
-   ".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox",
-   ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt",
-   "pptx"],
-   "ARCHIVES": [".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",
-   ".dmg", ".rar", ".xar", ".zip"],
-   "AUDIO": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
+    "DOCUMENTS":[".doc",'.pages','.fdf'],
+    "AUDIO":[".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
    ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"],
-   "PLAINTEXT": [".txt", ".in", ".out"],
-   "PDF": [".pdf"],
-   "PYTHON": [".py"],
-   "XML": [".xml"],
-   "EXE": [".exe"],
-   "SHELL": [".sh"]
+   "PHOTOS":[".jpg",".jpeg",],
+   "PDF":[".pdf"]
 }
+file_formats = {file_format: directory
+    for directory, file_formats in directories.items()
+    for file_format in file_formats
+}
+print(file_formats)
+def create_folders():
+    for object in os.scandir():
+        if object.is_dir():
+            continue
+        file_name = Path(object)
+        #lower case and gets file type
+        file_type = file_name.suffix.lower() 
+        if file_type in file_formats:  
+            direc_path =  Path(file_formats[file_type])
+            direc_path.mkdir(exist_ok=True)
+            file_name.rename((direc_path.joinpath(file_name)))
+        elif file_type not in file_formats:
+            misc = "MISC"
+            direc_path =  Path(misc)
+            direc_path.mkdir(exist_ok=True)
+            file_name.rename((direc_path.joinpath(file_name)))
 
-FILE_FORMATS = {file_format: directory
-   for directory, file_formats in DIRECTORIES.items()
-   for file_format in file_formats}
 
-def organise_folder():
-   for entry in os.scandir():
-      if entry.is_dir():
-         continue
-      file_path = Path(entry)
-      file_format = file_path.suffix.lower()
-      if file_format in FILE_FORMATS:
-         directory_path = Path(FILE_FORMATS[file_format])
-         directory_path.mkdir(exist_ok=True)
-         file_path.rename(directory_path.joinpath(file_path))
-   try:
-      os.mkdir("OTHER-FILES")
-   except:
-      pass
-
-   for dir in os.scandir():
-      try:
-         if dir.is_dir():
-            os.rmdir(dir)
-         else:
-            os.rename(os.getcwd() + '/' + str(Path(dir)), os.getcwd() + '/OTHER-FILES/' + str(Path(dir)))
-      except:
-         pass
-if __name__ == "__main__":
-   organise_folder()
+create_folders()
