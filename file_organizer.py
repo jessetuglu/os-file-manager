bbbd3402eb17a91827da733fc58939c7ff55f2c3
Jesse Tuglu <jktuglu@gmail.com>
2021-02-02 13:56:35 -0500

    touch-up

--- file_organizer.py
+++ file_organizer.py
@@ -2,37 +2,41 @@ import os
 from pathlib import Path
 
 directories = {
-    "PYTHON":['.py'],
-    "PLAINTEXT":['.txt'],
-    "VIDEOS":['.avi','.flv','.wmv',".mov", ".mp4", ".webm", ".vob", ".mng",
+    "python":['.py'],
+    "text_files":['.txt'],
+    "videos":['.avi','.flv','.wmv',".mov", ".mp4", ".webm", ".vob", ".mng",
    ".qt", ".mpg", ".mpeg", ".3gp"],
-    "DOCUMENTS":[".doc",'.pages','.fdf'],
-    "AUDIO":[".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
+    "documents":[".doc",'.pages','.fdf', '.xlsx'],
+    "audio":[".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
    ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"],
-   "PHOTOS":[".jpg",".jpeg",],
-   "PDF":[".pdf"]
+   "photos":[".jpg",".jpeg",],
+   "pdfs":[".pdf"]
 }
 file_formats = {file_format: directory
     for directory, file_formats in directories.items()
     for file_format in file_formats
 }
-print(file_formats)
-def create_folders():
-    for object in os.scandir():
-        if object.is_dir():
-            continue
-        file_name = Path(object)
-        #lower case and gets file type
-        file_type = file_name.suffix.lower() 
-        if file_type in file_formats:  
-            direc_path =  Path(file_formats[file_type])
+def create_folders(dir):
+    dir = Path(dir)
+    status_message = "Running..."
+    print(status_message)
+    if os.path.isdir(dir):
+        for object in os.scandir(dir):
+            if object.is_dir():
+                continue
+            file_name = Path(object)
+            file_type = file_name.suffix.lower() 
+            if file_type in file_formats:  
+                direc_path = Path(os.path.join(dir,file_formats[file_type]))
+            elif file_type not in file_formats:
+                misc = "msc"
+                direc_path = Path(os.path.join(dir,misc))
             direc_path.mkdir(exist_ok=True)
-            file_name.rename((direc_path.joinpath(file_name)))
-        elif file_type not in file_formats:
-            misc = "MISC"
-            direc_path =  Path(misc)
-            direc_path.mkdir(exist_ok=True)
-            file_name.rename((direc_path.joinpath(file_name)))
+            file_name.rename(os.path.join(direc_path,os.path.basename(file_name)))
+        status_message = "Sort Successful."
+    else:
+        status_message = "Failed. Please enter a valid path."
+    print(status_message)
 
 
-create_folders()
+create_folders() #Pass your dir in as str param
